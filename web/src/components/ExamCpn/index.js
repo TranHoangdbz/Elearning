81c96a98892660ad114d0b57dba8168e3f995f83
Minhthang1506 <19522216@gm.uit.edu.vn>
2022-05-11 08:14:10 +0700

    .

--- web/src/components/ExamCpn/index.js
+++ web/src/components/ExamCpn/index.js
@@ -3,7 +3,7 @@ import styles from './examCpn.module.scss';
 function ExamCpn() {
     return (
         <div className={`${styles.examStyle}`}>
-            This is Exam Component with CSS
+            This is Exam Component with SCSS
         </div>
     )
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,13 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import { createRoot } from 'react-dom/client';
 
 const store = configureStore();
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-);
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Provider store={store}>
+  <App />
+</Provider>);
