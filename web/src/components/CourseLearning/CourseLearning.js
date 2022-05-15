8720cf4f82df6de3234e327b162ff7dcc86fa2d3
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2022-05-15 14:11:03 +0700

    layout course

--- web/src/components/CourseLearning/CourseLearning.js
+++ web/src/components/CourseLearning/CourseLearning.js
@@ -1,31 +1,44 @@
 import './CourseLearning.scss';
+import { Container, Grid } from '@mui/material'
 
 function CourseLearning() {
     return (
-        <div className='container'>
-            <div className="video-container">
-                <iframe 
-                    width="916" height="515" 
-                    src="https://www.youtube.com/embed/SlPhMPnQ58k" 
-                    title="YouTube video player" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen
-                >    
-                </iframe>
-                <div className="ask-answer">
-                    <div>
-                        Bài giảng này khúc kia là sao đấy ạ
+        <Container spacing={2} style={{ marginTop: '40px' }} maxWidth='xl'>
+            <Grid spacing={1} container>
+                <Grid lg={8}>
+                    {/* Layout left */}
+                    <div className="video-container">
+                        <iframe
+                            width="100%" height="515"
+                            src="https://www.youtube.com/embed/SlPhMPnQ58k"
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen
+                        >
+                        </iframe>
+                        <div style={{ backgroundColor: 'red' }}> 1</div>
+                        <div className="ask-answer">
+                            <div>
+                                Bài giảng này khúc kia là sao đấy ạ
+                            </div>
+                            <div>
+                                Bài giảng này khúc kia là sao đấy ạ
+                            </div>
+                            <div>
+                                Bài giảng này khúc kia là sao đấy ạ
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        Bài giảng này khúc kia là sao đấy ạ
+                </Grid>
+                <Grid lg={4}>
+                    {/* Layout right */}
+                    <div style={{ paddingLeft: '20px' }}>
+                        <div>.NET Advanced</div>
                     </div>
-                    <div>
-                        Bài giảng này khúc kia là sao đấy ạ
-                    </div>
-                </div>
-            </div>
-        </div>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
