8babc4c1bd9804e30e187e05d5155a64c1c52d9f
Minhthang1506 <19522216@gm.uit.edu.vn>
2022-05-16 14:04:54 +0700

    Add MUI

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,21 @@
-import ExamPage from './features/counter';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import ExamPage from "./features/counter";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: ["Montserrat", "cursive"].join(","),
+    },
+});
+
 
 function App() {
-  return (
-    <div>
-      <ExamPage/>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div>
+                <ExamPage />
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
