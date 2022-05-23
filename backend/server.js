1f5ae007151112a4ed76009a8289c08e9259daec
MSI\tieum <18521293@gm.uit.edu.vn>
2022-05-23 22:35:08 +0700

    chore: clean code

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,6 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('./models/user');
 
 const PORT = 32
 
@@ -17,48 +14,10 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
-const GoogleClientID = '127746184739-7rv5euqh360qrcdvbd5oh1vr4hg6lbag.apps.googleusercontent.com';
-const GoogleClientSecret = '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';
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: GoogleClientID,
-            clientSecret: GoogleClientSecret,
-            callbackURL: '/auth/google/callback'
-        }, (profile, done) => {
-            if (profile.id) {
-                User.findOne({ _id: profile.id })
-                    .then((existingUser) => {
-                        if (existingUser) {
-                            done(null, existingUser);
-                        } else {
-                            new User({
-                                email: profile.emails[0].value,
-                                fullName: profile.name.familyName + ' ' + profile.name.givenName
-                            })
-                                .save()
-                                .then(user => done(null, user));
-                        }
-                    })
-            }
-        }
-    )
-);
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
 const coursesRouter = require('./routes/courses')
 app.use('/courses', coursesRouter)
 
 const route = require('./routes');
-const { propfind } = require('./routes/courses');
 route(app);
 
 app.listen(PORT, () => console.log('Server Started'))
