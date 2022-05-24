25ce392441aeb61a9599716ff8adfc45aff09ed4
Minhthang1506 <19522216@gm.uit.edu.vn>
2022-05-25 00:09:33 +0700

    .

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,33 +1,5 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import routes from "../routes";
+import { combineReducers } from 'redux'
 
-import React from "react";
-
-// waiting
-const loading = (
-    <div>
-        <h1>Loading...</h1>
-    </div>
-);
-
-const Routers = () => {
-    return (
-        <React.Suspense fallback={loading}>
-            <Routes>
-                {routes.publicRoute.map((route, idx) => {
-                    return (
-                        route.element && (
-                            <Route
-                                key={idx}
-                                path={route.path}
-                                element={route.element}
-                            />
-                        )
-                    );
-                })}
-            </Routes>
-        </React.Suspense>
-    );
-};
-
-export default Routers;
+export default combineReducers({
+    // someSlice: someSlice
+})
--- web/src/routers/index.js
+++ web/src/routers/index.js
@@ -4,11 +4,8 @@ import routes from "../routes";
 import React from "react";
 
 // import page
-const ExamPage = React.lazy(() => import("../components/exam"));
 const EditCourses = React.lazy(() => import("../features/edit-del-courses/pages/EditCourses"));
-
 const HomeCoursePage = React.lazy(() => import("../components/homeCourseList"))
-const DemoCourse = React.lazy(() => import("../features/demo-course"));
 
 
 // waiting
@@ -23,10 +20,6 @@ const Routers = () => {
         <React.Suspense fallback={loading}>
             <Routes>
                 <Route path="/editcourses" name="EditCourses" element={<EditCourses />} />
-                {/* <Route path="/exam" name="Example" element={<DemoCourse />} />   */}
-                <Route index name="Example" element={<Navigate to="exam" />} />
-                {/* <Route path="/exam" name="Example" element={<ExamPage />} />
-                <Route index name="Example" element={<Navigate to="exam" />} /> */}
                 <Route path="/homeCourseList" name="HomeCourse" element={<HomeCoursePage />} />
                 <Route index name="HomeCourse" element={<Navigate to="homeCourseList" />} />
                 {routes.publicRoute.map((route, idx) => {
