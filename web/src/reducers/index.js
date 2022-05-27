d576d325fd4edcc50311db98d042eeefb668afeb
minhman727 <62245831+minhman727@users.noreply.github.com>
2022-05-27 18:09:23 +0700

    fix merged bugs

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
-import counter from "../features/counter/counterSlice";
 import coursesManager from "../features/coursesManager/coursesManagerSlice";
 
 const rootReducer = combineReducers({
-  counter: counter,
   coursesManager: coursesManager,
 });
 
