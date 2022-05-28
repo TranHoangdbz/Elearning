5ebe741f507bdae75b51a78e89ff40a977086544
hauja47 <hoangletrunghau01@gmail.com>
2022-05-29 03:00:45 +0700

    Create and update models
    - Create Comment, Discussion, Lesson, Passed, Rating
    
    - Update Course, Teacher

--- mobile/lib/data/models/course.dart
+++ mobile/lib/data/models/course.dart
@@ -1,43 +1,71 @@
+import 'lesson.dart';
+import 'teacher.dart';
 import 'base_model.dart';
+import 'discussion.dart';
+import 'rating.dart';
 
 class Course extends BaseModel {
+  final String courseCode;
   final String courseName;
-  final String? courseImage;
+  final String courseImage;
+  final String demoVideo;
+  final String category;
   final String description;
-  final String? teacher;
-  final List<Object>? discussions;
-  final List<num>? rating;
+  final Teacher teacher;
+  final List<Discussion>? discussion;
+  final List<Rating>? rating;
+  final List<Lesson> lessons;
 
   Course({
     String? id,
+    required this.courseCode,
     required this.courseName,
-    this.courseImage,
+    required this.courseImage,
+    required this.demoVideo,
+    required this.category,
     required this.description,
-    this.teacher,
-    this.discussions,
-    this.rating
+    required this.teacher,
+    this.discussion,
+    this.rating,
+    required this.lessons,
   }) : super(id);
 
   @override
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'courseName': courseName,
-      'courseImage': courseImage,
-      'description': description,
-      'teacher': teacher,
-      'discussions': discussions,
-      'rating': rating
-    };
-  }
+  Map<String, dynamic> toJson() => {
+        "_id": id,
+        "courseCode": courseCode,
+        "courseImage": courseImage,
+        "courseName": courseName,
+        "demoVideo": demoVideo,
+        "category": category,
+        "description": description,
+        "teacher": teacher.toJson(),
+        "discussion": discussion == null
+            ? null
+            : List<dynamic>.from(discussion!.map((x) => x.toJson())),
+        "rating": rating == null
+            ? null
+            : List<dynamic>.from(rating!.map((x) => x.toJson())),
+        "lessons": List<dynamic>.from(lessons.map((x) => x)),
+      };
 
   factory Course.fromJson(Map<String, dynamic> data) => Course(
-    id: data['id'],
-    courseName: data['courseName'],
-    courseImage: data['courseImage'],
-    description: data['description'],
-    teacher: data['teacher'],
-    discussions: data['discussions'],
-    rating: data['rating'],
-  );
+        id: data["_id"],
+        courseCode: data["courseCode"],
+        courseImage: data["courseImage"],
+        courseName: data["courseName"],
+        demoVideo: data["demoVideo"],
+        category: data["category"],
+        description: data["description"],
+        teacher: Teacher.fromJson(data["teacher"]),
+        discussion: data["discussion"] == null
+            ? null
+            : List<Discussion>.from(
+                data["discussion"].map((x) => Discussion.fromJson(x))),
+        rating: data["rating"] == null
+            ? null
+            : List<Rating>.from(data["rating"].map((x) => Rating.fromJson(x))),
+        lessons:
+            List<Lesson>.from(data["lessons"].map((x) => Lesson.fromJson(x))),
+      );
 }
--- mobile/lib/data/models/teacher.dart
+++ mobile/lib/data/models/teacher.dart
@@ -3,41 +3,39 @@ import 'base_model.dart';
 class Teacher extends BaseModel {
   final String fullName;
   final String email;
-  final String? phoneNumber;
-  final String? title;
-  final String? profilePicture;
-  final List<Object>? courses;
+  final String phoneNumber;
+  final String title;
+  final String profilePicture;
+  final List<String> courses;
 
   Teacher({
     String? id,
     required this.fullName,
     required this.email,
-    this.title,
-    this.phoneNumber,
-    this.profilePicture,
-    this.courses,
+    required this.phoneNumber,
+    required this.title,
+    required this.profilePicture,
+    required this.courses,
   }) : super(id);
 
-  @override
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'fullName': fullName,
-      'email': email,
-      'title': title,
-      'phoneNumber': phoneNumber,
-      'profilePicture': profilePicture,
-      'courses': courses,
-    };
-  }
-
   factory Teacher.fromJson(Map<String, dynamic> data) => Teacher(
-    id: data['id'],
-    fullName: data['courseName'],
-    email: data['email'],
-    title: data['title'],
-    phoneNumber: data['phoneNumber'],
-    profilePicture: data['profilePicture'],
-    courses: data['courses'],
-  );
+        id: data["_id"],
+        fullName: data["fullName"],
+        email: data["email"],
+        phoneNumber: data["phoneNumber"],
+        title: data["title"],
+        profilePicture: data["profilePicture"],
+        courses: List<String>.from(data["courses"].map((x) => x)),
+      );
+
+  @override
+  Map<String, dynamic> toJson() => {
+        "_id": id,
+        "fullName": fullName,
+        "email": email,
+        "phoneNumber": phoneNumber,
+        "title": title,
+        "profilePicture": profilePicture,
+        "courses": List<dynamic>.from(courses.map((x) => x)),
+      };
 }
