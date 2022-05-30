4b43b5ddb9f0a0a347ec9c28a0e746713f47afe1
hauja47 <hoangletrunghau01@gmail.com>
2022-05-31 02:06:35 +0700

    Update lesson.dart

--- mobile/lib/data/models/lesson.dart
+++ mobile/lib/data/models/lesson.dart
@@ -1,50 +1,44 @@
 import 'base_model.dart';
-import 'passed.dart';
 
 class Lesson extends BaseModel {
-  String lessonCode;
   String description;
   String video;
   List<String> quizz;
-  List<Passed> passed;
   String thumbnail;
   String name;
   int lessonVolume;
+  int? v;
 
   Lesson({
     String? id,
-    required this.lessonCode,
     required this.description,
     required this.video,
     required this.quizz,
-    required this.passed,
     required this.thumbnail,
     required this.name,
     required this.lessonVolume,
+    this.v,
   }) : super(id);
 
   factory Lesson.fromJson(Map<String, dynamic> json) => Lesson(
         id: json["_id"],
-        lessonCode: json["lessonCode"],
         description: json["description"],
         video: json["video"],
         quizz: List<String>.from(json["quizz"].map((x) => x)),
-        passed:
-            List<Passed>.from(json["passed"].map((x) => Passed.fromJson(x))),
         thumbnail: json["thumbnail"],
         name: json["name"],
+        v: json["__v"],
         lessonVolume: json["lessonVolume"],
       );
 
   @override
   Map<String, dynamic> toJson() => {
         "_id": id,
-        "lessonCode": lessonCode,
         "description": description,
         "video": video,
         "quizz": List<dynamic>.from(quizz.map((x) => x)),
-        "passed": List<dynamic>.from(passed.map((x) => x.toJson())),
         "thumbnail": thumbnail,
+        "__v": v,
         "name": name,
         "lessonVolume": lessonVolume,
       };
