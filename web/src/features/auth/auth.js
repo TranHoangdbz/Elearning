4915c9df5907767cba199b9257bf22495a8d8eeb
nguyentan2212 <nguyentan2212@gmail.com>
2022-06-04 18:33:54 +0700

    chore: call to test api

--- web/src/features/auth/auth.js
+++ web/src/features/auth/auth.js
@@ -1,4 +1,5 @@
 import API from "../../services";
+import { getToken } from "./localStorage";
 
 export const register = async (email, fullName, phoneNumber, password) => {
     const result = API.post('http://localhost:32/api/users/register', {email, fullName, phoneNumber, password});
@@ -8,4 +9,10 @@ export const register = async (email, fullName, phoneNumber, password) => {
 export const signin = async (email, password) => {
     const result = API.post('http://localhost:32/api/users/login', {email, password});
     return result;
+}
+
+export const callTest = async () => {
+    const token = getToken();
+    const result = API.get('http://localhost:32/api/users/test', null, { headers: {"procources-access-token" : `${token}`} });
+    console.log(result);
 }
