620f01193d09a42ee39266e0655a88dc1a8bd433
nguyentan2212 <nguyentan2212@gmail.com>
2022-06-04 18:31:02 +0700

    chore: add test route for auth

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
+const auth = require("../middleware/auth");
 
-router.get('/verify', userController.verifyUser);
-router.post('/register', userController.register);
-router.post('/login', userController.login);
+router.get("/verify", userController.verifyUser);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get(
+  "/test",
+  auth,
+  userController.test
+);
 
 module.exports = router;
