a73ecd201417d51e8d86a52861804b68341aa368
hauja47 <hoangletrunghau01@gmail.com>
2022-06-07 01:07:05 +0700

    Update lessons screen

--- mobile/lib/modules/lessons/controllers/lessons_controller.dart
+++ mobile/lib/modules/lessons/controllers/lessons_controller.dart
@@ -2,15 +2,4 @@ import 'package:get/get.dart';
 import 'package:uit_elearning/data/models/lesson.dart';
 import 'package:uit_elearning/data/services/lesson_service.dart';
 
-class LessonsController extends GetxController {
-
-  List<Lesson> getCourseByCategory(String category) {
-    return LessonService.instance
-        .getAllLessons()
-        .toList();
-  }
-
-  getAllLessons() async {
-    return await LessonService.instance.fetchLessons();
-  }
-}
+class LessonsController extends GetxController {}
--- mobile/lib/modules/lessons/screens/lessons_screen.dart
+++ mobile/lib/modules/lessons/screens/lessons_screen.dart
@@ -9,6 +9,7 @@ import 'package:uit_elearning/constants/text_styles.dart';
 import 'package:uit_elearning/data/models/course.dart';
 import 'package:uit_elearning/global_widgets/course_list.dart';
 import 'package:uit_elearning/global_widgets/custom_icon_button.dart';
+import 'package:uit_elearning/global_widgets/lesson_item.dart';
 import 'package:uit_elearning/modules/lessons/controllers/lessons_controller.dart';
 
 class LessonsScreen extends GetView<LessonsController> {
@@ -19,150 +20,135 @@ class LessonsScreen extends GetView<LessonsController> {
 
   @override
   Widget build(BuildContext context) {
-      Get.put(LessonsController());
-      return Scaffold(
-        body: Container(
-          width: double.maxFinite,
-          height: double.maxFinite,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        course.courseName,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20,
-                        ),
-                      ),
-                      SizedBox(height: 20,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("10k students",
-                                style: TextStyles.textStyleOnBackgroundColor14w600,
-                              ),
-                              SizedBox(height: 20,),
-                              Row(
-                                children: [
-                                  Container(
-                                    child: CircleAvatar(
-                                      backgroundImage: const NetworkImage(
-                                          'https://picsum.photos/100/100?image=35'),
-                                      onBackgroundImageError: (e, s) {
-                                        debugPrint('Teacher avatar can\'t load: , $e, $s');
-                                      },
-                                      minRadius: 15,
-                                    ),
-                                  ),
-                                  SizedBox(width: 10,),
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(course.teacher.fullName,
-                                        style: TextStyles.textStyleOnBackgroundColor14w600,
-                                      ),
-                                      Text(course.teacher.title,
-                                          style: TextStyles.textStylePrimaryColor12w300
-                                      )
-                                    ],
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Row(
-                                children: [
-                                  Text("4.8",
-                                    style: TextStyles.textStyleOnBackgroundColor14w600,
-                                  ),
-                                  SizedBox(width: 10,),
-                                  Icon(
-                                    Icons.star,
-                                    color: Colors.yellow,
-                                  )
-                                ],
-                              ),
-                              Text("123 reviews",
-                                style: TextStyles.textStyleOnBackgroundColor14w500,
-                              ),
-                              SizedBox(height: 10,),
-                              Text("Enroll",
-                                style: TextStyles.textStyleSecondaryLightColor14w600,
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Text(
-                        course.description,
-                        style: TextStyles.textStyleOnBackgroundColor12w400,
-                      )
-                    ],
-                  ),
-                ),
-                Column(
+    Get.put(LessonsController());
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      child: FutureBuilder(
-                        future: controller.getAllLessons(),
-                        builder: (BuildContext context, snapshot) {
-                          if (snapshot.hasData) {
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: controller
-                                  .getAllLessons()
-                                  .map<Widget>(
-                                    (category) => Column(
-                                  children: [
-                                      CourseList(
-                                      courses:
-                                      controller.getAllLessons(),
-                                      category: category,
-                                    ),
-                                    const SizedBox(
-                                      height: 32,
-                                    ),
-                                  ],
-                                ),
-                              )
-                                  .toList(),
-                            );
-                          } else if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          } else if (snapshot.hasError) {
-                            return const Center(
-                              child: Text('An error has occured'),
-                            );
-                          } else {
-                            return const Center(
-                              child: Text('Empty'),
-                            );
-                          }
-                        },
+                    Text(
+                      course.courseName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
                       ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "10k students",
+                              style:
+                                  TextStyles.textStyleOnBackgroundColor14w600,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  child: CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        course.teacher.profilePicture),
+                                    onBackgroundImageError: (e, s) {
+                                      debugPrint(
+                                          'Teacher avatar can\'t load: , $e, $s');
+                                    },
+                                    minRadius: 15,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      course.teacher.fullName,
+                                      style: TextStyles
+                                          .textStyleOnBackgroundColor14w600,
+                                    ),
+                                    Text(course.teacher.title,
+                                        style: TextStyles
+                                            .textStylePrimaryColor12w300)
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "4.8",
+                                  style: TextStyles
+                                      .textStyleOnBackgroundColor14w600,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                )
+                              ],
+                            ),
+                            Text(
+                              "123 reviews",
+                              style:
+                                  TextStyles.textStyleOnBackgroundColor14w500,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Enroll",
+                              style:
+                                  TextStyles.textStyleSecondaryLightColor14w600,
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      course.description,
+                      style: TextStyles.textStyleOnBackgroundColor12w400,
                     )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              Column(
+                children: course.lessons
+                    .map((lesson) => Container(
+                          margin: const EdgeInsets.only(bottom: 20),
+                          child: LessonItem(lesson: lesson),
+                        ))
+                    .toList(),
+              )
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
 }
