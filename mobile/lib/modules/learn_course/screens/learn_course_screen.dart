4fcefa69bc12cd82cba2710b09bc499697c6646a
hauja47 <hoangletrunghau01@gmail.com>
2022-06-07 01:06:40 +0700

    Update learn_course_screen.dart

--- mobile/lib/modules/learn_course/screens/learn_course_screen.dart
+++ mobile/lib/modules/learn_course/screens/learn_course_screen.dart
@@ -6,6 +6,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:uit_elearning/data/models/course.dart';
+import 'package:uit_elearning/data/models/lesson.dart';
 import 'package:uit_elearning/global_widgets/custom_icon_button.dart';
 import 'package:uit_elearning/global_widgets/video_player_wiget.dart';
 import 'package:uit_elearning/modules/disscussion/screens/disscussion_screen.dart';
@@ -16,7 +17,9 @@ import 'package:video_player/video_player.dart';
 
 class LearnCourseScreen extends StatelessWidget {
   final Course course;
-  LearnCourseScreen({Key? key, required this.course}) : super(key: key);
+  final Lesson lesson;
+  LearnCourseScreen({Key? key, required this.course, required this.lesson})
+      : super(key: key);
 
   //final _controller = Get.find<LearnCoursesController>();
   late VideoPlayerController vidcontroller;
@@ -24,115 +27,117 @@ class LearnCourseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Get.put(LearnCoursesController());
-    vidcontroller = VideoPlayerController.network("https://www.youtube.com/watch?v=sMcfFmR0MmA");
-    return GetBuilder<LearnCoursesController>(
-        builder: (_controller) {
-          return DefaultTabController(
-              length: 3,
-              child: SafeArea(
-                child: Scaffold(
-                  appBar: PreferredSize(
-                    preferredSize: Size.fromHeight(270),
-                    child: Container(
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 210,
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
+    vidcontroller = VideoPlayerController.network(
+        // "https://www.youtube.com/watch?v=sMcfFmR0MmA"
+        lesson.video);
+    return GetBuilder<LearnCoursesController>(builder: (_controller) {
+      return DefaultTabController(
+          length: 3,
+          child: SafeArea(
+            child: Scaffold(
+              appBar: PreferredSize(
+                preferredSize: Size.fromHeight(270),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 210,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0,
+                                child: Container(
+                                  height: 250,
+                                  color: Colors.red,
+                                  child: VideoPlayerWidget(
+                                    controller: vidcontroller,
+                                  ),
+                                )),
+                            Positioned(
+                              left: 0,
+                              top: 0,
+                              child: CustomIconButton(
+                                  icon: const Icon(
+                                    Icons.arrow_back_ios_new_rounded,
+                                    color: Colors.white,
+                                  ),
+                                  onTap: () {
+                                    Get.back();
+                                  }),
                             ),
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                    left: 0,
-                                    right: 0,
-                                    top: 0,
-                                    bottom: 0,
-                                    child: Container(
-                                      height: 250,
-                                      color: Colors.red,
-                                      child: VideoPlayerWidget(controller: vidcontroller,),
-                                    )),
-                                Positioned(
-                                  left: 0,
-                                  top: 0,
-                                  child: CustomIconButton(
+                            Positioned(
+                              left: 2,
+                              bottom: 5,
+                              child: Row(
+                                children: [
+                                  CustomIconButton(
                                       icon: const Icon(
-                                        Icons.arrow_back_ios_new_rounded,
+                                        Icons.fast_rewind,
                                         color: Colors.white,
                                       ),
-                                      onTap: (){
+                                      onTap: () {
                                         Get.back();
-                                      }
-                                  ),
-                                ),
-                                Positioned(
-                                  left: 2,
-                                  bottom: 5,
-                                  child: Row(
-                                    children: [
-                                      CustomIconButton(
-                                          icon: const Icon(
-                                            Icons.fast_rewind,
-                                            color: Colors.white,
-                                          ),
-                                          onTap: (){
-                                            Get.back();
-                                          }
+                                      }),
+                                  CustomIconButton(
+                                      icon: const Icon(
+                                        Icons.pause,
+                                        color: Colors.white,
                                       ),
-                                      CustomIconButton(
-                                          icon: const Icon(
-                                            Icons.pause,
-                                            color: Colors.white,
-                                          ),
-                                          onTap: (){
-                                            Get.back();
-                                          }
+                                      onTap: () {
+                                        Get.back();
+                                      }),
+                                  CustomIconButton(
+                                      icon: const Icon(
+                                        Icons.fast_forward,
+                                        color: Colors.white,
                                       ),
-                                      CustomIconButton(
-                                          icon: const Icon(
-                                            Icons.fast_forward,
-                                            color: Colors.white,
-                                          ),
-                                          onTap: (){
-                                            Get.back();
-                                          }
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
+                                      onTap: () {
+                                        Get.back();
+                                      }),
+                                ],
+                              ),
                             ),
-                          ),
-
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: TabBar(
-                              unselectedLabelColor: Colors.black,
-                              labelColor: Colors.black,
-                              indicatorColor: Colors.black,
-                              tabs: [
-                                Tab(text: 'Lessons',),
-                                Tab(text: 'Quizzes',),
-                                Tab(text: 'Disscussion',),
-                              ],
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
-                  body: TabBarView(
-                    children: [
-                      LessonsScreen(course),
-                      MyCoursesScreen(),
-                      DisscussionScreen(course),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: TabBar(
+                          unselectedLabelColor: Colors.black,
+                          labelColor: Colors.black,
+                          indicatorColor: Colors.black,
+                          tabs: [
+                            Tab(
+                              text: 'Lessons',
+                            ),
+                            Tab(
+                              text: 'Quizzes',
+                            ),
+                            Tab(
+                              text: 'Disscussion',
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              )
-          );
-        }
-    );
+              ),
+              body: TabBarView(
+                children: [
+                  LessonsScreen(course),
+                  MyCoursesScreen(),
+                  DisscussionScreen(course),
+                ],
+              ),
+            ),
+          ));
+    });
   }
 }
